871066e7a0cb690bbfaed7ff9edc05d0e3ea4c8f
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-16 01:26:53 +0100

    TASKS

--- My Project/reflection/person.py
+++ My Project/reflection/person.py
@@ -1,9 +1,19 @@
-import problem
+from problem import Problem
 
 
 class Person:
-    def problem_list(self):
-        problems = list(problem)
+    def __init__(self):
+        self.problem_list = []
 
-    def addProblem(self, problem):
-        problem_list.problems(problem)
+    def add_problem(self, problem_to_add: Problem):
+        self.problem_list.append(problem_to_add)
+
+    def solve_problem(self, problem_to_solve):
+        if problem_to_solve in self.problem_list:
+            self.problem_list.remove(problem_to_solve)
+            return "Problem solved"
+        return "problem not in the list of problems"
+
+    def display_problems(self):
+        return self.problem_list
+
