39f0317707ede17650555b43435775bb3da5981f
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-16 16:42:39 +0100

    TASKS

--- Account.py
+++ Account.py
@@ -4,14 +4,22 @@ from decimal import Decimal
 class Account:
     def __init__(self, name, balance: Decimal):
         self.name = name
-        self.balance = balance
+        self.__balance = balance
 
     @property
     def balance(self):
-        return self.balance
+        return self.__balance
 
     @balance.setter
     def balance(self, balance):
         if balance < Decimal(0.00):
             raise ValueError("Invalid Balance for balance")
-        self.balance = balance
+        self.__balance = balance
+
+    def __str__(self):
+        return f"Name: {self.name}, Balance: {self.__balance}"
+
+
+ai = Account("Dayone", Decimal(-10))
+
+print(ai)
