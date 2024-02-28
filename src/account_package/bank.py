bb51de607052105680788df99581c4476c6e44b6
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-28 08:56:58 +0100

    TASKS

--- src/account_package/bank.py
+++ src/account_package/bank.py
@@ -12,37 +12,36 @@ class BankAccount:
         self._account_list.append(Account(account_name, pin, self.account_number_generator + 1))
 
     def deposit(self, account_number, amount):
+        account = self.__find_account(account_number)
+        account.deposit(amount)
+
+    def __find_account(self, account_number):
         for account in self._account_list:
             if account_number == account.get_account_number():
-                account.deposit(amount)
+                return account
+        raise ValueError(f"Account number {account_number} not found")
 
     def withdraw(self, account_number, amount, pin):
-        for account in self._account_list:
-            if account_number == account.get_account_number():
-                account.withdraw(amount, pin)
+        account = self.__find_account(account_number)
+        account.withdraw(amount, pin)
 
     def get_number_of_accounts(self):
         return len(self._account_list)
 
     def transfer(self, sender, receiver, amount, pin):
-        for account in self._account_list:
-            if account.get_account_number() == sender:
-                account.withdraw(amount, pin)
-        for account in self._account_list:
-            if account.get_account_number() == receiver:
-                account.deposit(amount)
+        sender_account = self.__find_account(sender)
+        sender_account.withdraw(amount, pin)
+        receiver_account = self.__find_account(receiver)
+        receiver_account.deposit(amount)
 
     def remove_account(self, account_number, pin):
-        for account in self._account_list:
-            if account.get_account_number() == account_number:
-                account.is_valid_pin(pin)
-                if account.get_account_number() == account_number:
-                    self._account_list.remove(account)
+        account = self.__find_account(account_number)
+        account.is_valid_pin(pin)
+        self._account_list.remove(account)
 
     def check_balance(self, account_number, pin):
-        for account in self._account_list:
-            if account.get_account_number() == account_number:
-                return f"{account.check_balance(pin)}"
+        account = self.__find_account(account_number)
+        return f"{account.check_balance(pin)}"
 
 
 
