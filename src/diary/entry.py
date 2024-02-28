a98ecf0300620b36170a02471073e282d15fdfdc
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-28 08:24:32 +0100

    TASKS

---
+++ src/diary/entry.py
@@ -0,0 +1,11 @@
+class Entry:
+
+    def __init__(self, id, title, body):
+        self.body = body
+        self._id = id
+
+    def get_id(self):
+        return self._id
+    
+    def get_body(self):
+        return self.body
