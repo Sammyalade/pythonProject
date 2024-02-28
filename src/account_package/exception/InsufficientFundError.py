b054077eb7e950cbf6b5b628aad4222b4fa045c5
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-28 08:57:20 +0100

    TASKS

--- src/account_package/exception/InsufficientFundError.py
+++ src/account_package/exception/InsufficientFundError.py
@@ -2,3 +2,6 @@ class InsufficientFundError(BaseException):
 
     def __init__(self, message="Invalid Amount"):
         super().__init__(message)
+
+    def __str__(self):
+        return f"Insufficient Fund"
