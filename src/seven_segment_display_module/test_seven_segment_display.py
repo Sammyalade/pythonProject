817f59420c0eecdf8e7d6508f63f7abc2a70a0ea
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 09:31:06 +0100

    TASKS

--- src/seven_segment_display_module/test_seven_segment_display.py
+++ src/seven_segment_display_module/test_seven_segment_display.py
@@ -10,3 +10,12 @@ class TestSevenSegmentDisplay:
         with pytest.raises(RuntimeError):
             obj.put_in_a_list()
 
+    def test_that_when_you_put_less_than_8_exception_is_thrown(self):
+        obj = seven_segment_display.SevenSegmentDisplay("1000")
+        with pytest.raises(RuntimeError):
+            obj.put_in_a_list()
+
+    def test_when_you_put_any_number_aside_zero_and_one_exception_is_thrown(self):
+        obj = seven_segment_display.SevenSegmentDisplay("10001122")
+        with pytest.raises(ValueError):
+            obj.put_in_a_list()
