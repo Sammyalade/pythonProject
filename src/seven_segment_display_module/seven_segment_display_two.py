2d2777bf51b823c709b37c22f78659863af921ca
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 17:35:40 +0100

    TASKS

--- src/seven_segment_display_module/seven_segment_display_two.py
+++ src/seven_segment_display_module/seven_segment_display_two.py
@@ -5,27 +5,29 @@ class SevenSegmentDisplayTwo:
         self.number_segment = number_segment
         self.segments = {}
 
-    @staticmethod
-    def print_horizontal():
-        horizontal = "*  *  *  *"
+    def print_horizontal(self):
+        return f"*  *  *  *"
 
     @staticmethod
     def print_vertical_left():
-        vertical  1 = "*"
-        vertical2 = "*"
-        vertical3 = "*"
+        vertical_left1 = "*"
+        vertical_left2 = "*"
+        vertical_left3 = "*"
+        return f"{vertical_left1}\n{vertical_left2}\n{vertical_left3}\n"
 
     @staticmethod
     def print_vertical_right():
-        "         *")
-        print("         *")
-        print("         *")
+        vertical_right1 = "         *"
+        vertical_right2 = "         *"
+        vertical_right3 = "         *"
+        return f"{vertical_right1}\n{vertical_right2}\n{vertical_right3}\n"
 
     @staticmethod
     def print_vertical_right_and_left():
-        print("*        *")
-        print("*        *")
-        print("*        *")
+        vertical_right_and_left1 = "*        *"
+        vertical_right_and_left2 = "*        *"
+        vertical_right_and_left3 = "*        *"
+        return f"{vertical_right_and_left1}\n{vertical_right_and_left2}\n{vertical_right_and_left3}\n"
 
     def put_in_a_list(self):
         my_list = []
@@ -34,18 +36,18 @@ class SevenSegmentDisplayTwo:
             self.count += 1
             if number not in ['0', '1']:
                 raise ValueError(f"Character at {self.count} is invalid")
-        if len(my_list) > 7 or len(my_list) < 7:
+        if len(my_list) > 8 or len(my_list) < 8:
             raise RuntimeError("Input should not be more than or less than 7 numbers")
         return my_list
 
     def print(self):
         list1 = self.put_in_a_list()
         if list1[7] == '1':
-            self.control_horizontal_printing(list1[0])
-            self.control_vertical_printing(list1[1], list1[5])
-            self.control_horizontal_printing(list1[6])
-            self.control_vertical_printing(list1[2], list1[4])
-            self.control_horizontal_printing(list1[3])
+            print(self.control_horizontal_printing(list1[0]))
+            print(self.control_vertical_printing(list1[1], list1[5]))
+            print(self.control_horizontal_printing(list1[6]))
+            print(self.control_vertical_printing(list1[2], list1[4]))
+            print(self.control_horizontal_printing(list1[3]))
 
     def control_horizontal_printing(self, switch: str):
         if switch == '1':
