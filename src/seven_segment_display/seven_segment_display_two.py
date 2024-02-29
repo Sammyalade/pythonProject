b3be5bf50a6d3aaa4e950e46ab80e64ce5ab08ce
Sammyalade <olufemialadeyelu@gmail.com>
2024-02-29 09:05:02 +0100

    TASKS

--- src/seven_segment_display/seven_segment_display_two.py
+++ src/seven_segment_display/seven_segment_display_two.py
@@ -1,29 +1,65 @@
-class SevenSegmentDisplayTwo:
+class SevenSegmentDisplay:
 
-    def __init__(self, switch):
-        self.switch = switch
-        self.my_dict = {}
+    def __init__(self, number_segment: str):
         self.count = 0
-        self.display = [["*  *  *  *"], ["*"], ["*"], ["*"]]
+        self.number_segment = number_segment
+        self.segments = {}
 
-    def initialize_dictionary(self):
-        for letter, char in zip("abcdefgh", self.switch):
+    @staticmethod
+    def print_horizontal():
+        print("*  *  *  *")
+
+    @staticmethod
+    def print_vertical_left():
+        print("*")
+        print("*")
+        print("*")
+
+    @staticmethod
+    def print_vertical_right():
+        print("         *")
+        print("         *")
+        print("         *")
+
+    @staticmethod
+    def print_vertical_right_and_left():
+        print("*        *")
+        print("*        *")
+        print("*        *")
+
+    def put_in_a_list(self):
+        my_list = []
+        my_list += self.number_segment
+        for number in my_list:
             self.count += 1
-            check_number = int(char)
-            if check_number > 1 or check_number < 0:
+            if number not in ['0', '1']:
                 raise ValueError(f"Character at {self.count} is invalid")
-            self.my_dict[letter] = check_number
-        print(self.my_dict)
+        if len(my_list) > 7 or len(my_list) < 7:
+            raise RuntimeError("Input should not be more than or less than 7 numbers")
+        return my_list
 
-    def control_print(self):
-        for i in self.my_dict:
-            if self.my_dict['h'] == 1:
-                if self.my_dict['a'] == 1:
-                    print(self.display[0][0])
+    def print(self):
+        list1 = self.put_in_a_list()
+        if list1[7] == '1':
+            self.control_horizontal_printing(list1[0])
+            self.control_vertical_printing(list1[1], list1[5])
+            self.control_horizontal_printing(list1[6])
+            self.control_vertical_printing(list1[2], list1[4])
+            self.control_horizontal_printing(list1[3])
+
+    def control_horizontal_printing(self, switch: str):
+        if switch == '1':
+            self.print_horizontal()
+
+    def control_vertical_printing(self, switch1, switch2):
+        if switch1 == '1' and switch2 == '1':
+            self.print_vertical_right_and_left()
+        elif switch2 == '1' and switch1 == '0':
+            self.print_vertical_left()
+        elif switch2 == '0' and switch1 == '1':
+            self.print_vertical_right()
 
 
 if __name__ == '__main__':
-    segment = SevenSegmentDisplayTwo("01100101")
-    segment.initialize_dictionary()
-    #segment.control_printing()
-
+    segment = SevenSegmentDisplay("11110111")
+    segment.print()
