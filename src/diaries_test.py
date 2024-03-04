0a0c7359eb8791720131ae5bbbc763f6548248c2
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 12:19:33 +0100

    TASKS

--- src/diaries_test.py
+++ src/diaries_test.py
@@ -1,7 +1,17 @@
-import unittest
+from diary_module.diaries import Diaries
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def test_add_diaries():
+    diaries = Diaries()
+    diaries.add("username", "password")
+    assert diaries.getNumberOfDiaries() == 1
+
+
+def test_delete_diaries():
+    diaries = Diaries()
+    diaries.add("username", "password")
+    diaries.delete("username", "password")
+    assert diaries.getNumberOfDiaries() == 0
+
+def test_find_diaries_by_username():
 
