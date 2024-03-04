f53792cb0ce9a4b6013252c265772b634effeffe
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 15:52:13 +0100

    TASKS

--- src/diaries_test.py
+++ src/diaries_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from diary_module.diaries import Diaries
 from diary_module.diary import Diary
 
@@ -20,3 +22,11 @@ def test_find_diaries_by_username():
     diaries.add("username", "password")
     assert isinstance(diaries.find("username"), Diary)
 
+
+def test_delete_diaries_find_diaries_raises_file_not_found_error():
+    diaries = Diaries()
+    diaries.add("username", "password")
+    diaries.add("username1", "password1")
+    diaries.delete("username1", "password1")
+    with pytest.raises(FileNotFoundError):
+        assert diaries.find("username1")
