6e19de471fe6a324d34d33bcee4fa03d423be3a9
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 14:26:00 +0100

    TASKS

--- src/diaries_test.py
+++ src/diaries_test.py
@@ -1,4 +1,5 @@
 from diary_module.diaries import Diaries
+from diary_module.diary import Diary
 
 
 def test_add_diaries():
@@ -13,5 +14,9 @@ def test_delete_diaries():
     diaries.delete("username", "password")
     assert diaries.getNumberOfDiaries() == 0
 
+
 def test_find_diaries_by_username():
+    diaries = Diaries()
+    diaries.add("username", "password")
+    assert isinstance(diaries.find("username"), Diary)
 
