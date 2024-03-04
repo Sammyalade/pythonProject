2dffb1558bfa1b8ee8824a6d79ad8707bdb3321b
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 16:16:58 +0100

    TASKS

--- src/diaries_test.py
+++ src/diaries_test.py
@@ -4,29 +4,38 @@ from diary_module.diaries import Diaries
 from diary_module.diary import Diary
 
 
-def test_add_diaries():
-    diaries = Diaries()
-    diaries.add("username", "password")
-    assert diaries.getNumberOfDiaries() == 1
+class TestDiaries:
 
+    def test_add_diaries(self):
+        diaries = Diaries()
+        diaries.add("username", "password")
+        assert diaries.getNumberOfDiaries() == 1
 
-def test_delete_diaries():
-    diaries = Diaries()
-    diaries.add("username", "password")
-    diaries.delete("username", "password")
-    assert diaries.getNumberOfDiaries() == 0
+    def test_delete_diaries(self):
+        diaries = Diaries()
+        diaries.add("username", "password")
+        diaries.delete("username", "password")
+        assert diaries.getNumberOfDiaries() == 0
 
+    def test_find_diaries_by_username(self):
+        diaries = Diaries()
+        diaries.add("username", "password")
+        assert isinstance(diaries.find("username"), Diary)
 
-def test_find_diaries_by_username():
-    diaries = Diaries()
-    diaries.add("username", "password")
-    assert isinstance(diaries.find("username"), Diary)
+    def test_delete_diaries_find_diaries_raises_file_not_found_error(self):
+        diaries = Diaries()
+        diaries.add("username", "password")
+        diaries.add("username1", "password1")
+        diaries.delete("username1", "password1")
+        with pytest.raises(FileNotFoundError):
+            assert diaries.find("username1")
 
+    def test_create_diary_with_empty_username_throws_exception(self):
+        diaries = Diaries()
+        with pytest.raises(ValueError):
+            diaries.add("", "")
 
-def test_delete_diaries_find_diaries_raises_file_not_found_error():
-    diaries = Diaries()
-    diaries.add("username", "password")
-    diaries.add("username1", "password1")
-    diaries.delete("username1", "password1")
-    with pytest.raises(FileNotFoundError):
-        assert diaries.find("username1")
+    def test_create_diary_with_empty_password_throws_exception(self):
+        diaries = Diaries()
+        with pytest.raises(ValueError):
+            diaries.add("username", "")
