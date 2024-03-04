e07be3c8dd384a40770430b69abd821902542620
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-04 11:25:21 +0100

    TASKS

--- src/miscellanous/letters_and_digit.py
+++ src/miscellanous/letters_and_digit.py
@@ -12,7 +12,7 @@ def count_letters_and_digits(sentence: str):
 print(count_letters_and_digits("hello world! 123"))
 
 
-def count_letters_and_digitsTwo(sentence: str):
+def count_letters_and_digits_two(sentence: str):
     letters = 0
     digits = 0
     for word in sentence:
@@ -23,4 +23,12 @@ def count_letters_and_digitsTwo(sentence: str):
     return {"LETTERS": letters, "DIGITS": digits}
 
 
-print(count_letters_and_digitsTwo("hello world! 123"))
+print(count_letters_and_digits_two("hello world! 123"))
+
+
+def count_letters_and_digits_three(sentence: str):
+    return {"LETTERS": len(list(filter(lambda letter: letter.isalpha(), sentence))),
+            "DIGITS": len(list(filter(lambda letter: letter.isdigit(), sentence)))}
+
+
+print(count_letters_and_digits_three("hello world! 123"))
